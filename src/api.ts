6e75fca22ca061f390677f6d76557a3336375ba2
Richard Morales <richmorales@nologis.com>
2019-08-05 10:29:53 +0100

    change togg-lib for weekly reports

--- src/api.ts
+++ src/api.ts
@@ -4,7 +4,7 @@ import { SummaryByProject } from './models';
 
 const VERSION = require('../package.json').version;
 
-const base = token => workspace => endpoint => {
+const base = token => workspace => sinceDate => untilDate =>  endpoint => {
   const config: AxiosRequestConfig = {
     url: `https://toggl.com/${endpoint}`,
     method: 'GET',
@@ -17,20 +17,22 @@ const base = token => workspace => endpoint => {
     },
     params: {
       user_agent: `toggl-lib-${VERSION}`,
-      workspace_id: workspace
+      workspace_id: workspace,
+      since: sinceDate,
+      until: untilDate
     }
   };
   return axios.request(config).catch(console.error);
 };
 
 const toProjectSummary = (summary): SummaryByProject => ({
-  client: summary.title.client,
-  project: summary.title.project,
-  time: humanizeMilliseconds(summary.time)
+  client: summary.title.client && summary.title.client.toUpperCase(),
+  project: summary.title.project && summary.title.project.toUpperCase(),
+  time: summary.totals[summary.totals.length -1]
 });
 
 const parseSummaryByProjects = result => result.data.data.map(toProjectSummary);
 
-export const api = (token, workspace) => ({
-  summaryByProjects: base(token)(workspace)('reports/api/v2/summary?grouping=projects').then(parseSummaryByProjects)
+export const api = (token, workspace, sinceDate, untilDate) => ({
+  summaryByProjects: base(token)(workspace)(sinceDate)(untilDate)('reports/api/v2/weekly?grouping=projects').then(parseSummaryByProjects)
 });
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { api } from './api';
 import { SummaryByProject } from './models';
 
-export const client = (token: string, workspace: string) => ({
-  getSummaryGroupByClients: (): Promise<SummaryByProject[]> => api(token, workspace).summaryByProjects
+export const client = (token: string, workspace: string, sinceDate: string, untilDate: string) => ({
+  getSummaryGroupByClients: (): Promise<SummaryByProject[]> => api(token, workspace, sinceDate, untilDate).summaryByProjects
 });
