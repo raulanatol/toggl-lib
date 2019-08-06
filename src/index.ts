eee06a94e9b27b42885e7fdc56ce3029ded8c3a4
Richard Morales <richmorales@nologis.com>
2019-08-06 08:28:53 +0100

    remove untilDate

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import { api } from './api';
 import { SummaryByProject } from './models';
 
 export const client = (token: string, workspace: string) => ({
-  getSummaryGroupByClients: (sinceDate: string, untilDate: string): Promise<SummaryByProject[]> => api(token, workspace).summaryByProjects(sinceDate)
+  getSummaryGroupByClients: (sinceDate: string): Promise<SummaryByProject[]> => api(token, workspace).summaryByProjects(sinceDate)
 });
